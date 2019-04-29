7d07e980f82a05c4b0c3ae772ab4d9e59adb5a45
litalsh12 <44766198+litalsh12@users.noreply.github.com>
2019-04-29 20:25:57 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -26,8 +26,8 @@ int main() {
 	if (signal == 0) {
 
 		// BASIC TESTS - DO NOT CHANGE
-		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"};
-		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"};
+    ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"},c98a7{"98a7"},c123{"123"},c1111{"1111"};
+		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"},g98a7{"98a7"},g123{"123"},g1111{"1111"}; 
 
 
 		testcase.setname("Calculate bull and pgia")
@@ -60,8 +60,6 @@ int main() {
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","657321"), "5,0")    // 5 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","894321"), "4,0")    // 4 bull, 0 pgia
 	
-	
-
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","123456"), "0,6")    // 0 bull, 6 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","123000"), "0,3")    // 0 bull, 3 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","666666"), "1,5")    // 1 bull, 5 pgia
@@ -69,7 +67,13 @@ int main() {
 		.CHECK_OUTPUT(calculateBullAndPgia("654321","098790"), "0,0")    // 0 bull, 0 pgia
 	
 		.CHECK_THROWS(calculateBullAndPgia("654asa321","098ss0"))
-
+		.CHECK_THROWS(calculateBullAndPgia("6542234","012"))
+		.CHECK_THROWS(calculateBullAndPgia("abc","abc"))
+		.CHECK_THROWS(calculateBullAndPgia("654_34","098123"))
+		.CHECK_THROWS(calculateBullAndPgia("65 63","6563"))
+		.CHECK_THROWS(calculateBullAndPgia("6321","abcd"))
+		.CHECK_THROWS(calculateBullAndPgia("6321","6 321"))
+		.CHECK_THROWS(calculateBullAndPgia("654","0+k"))
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
@@ -77,6 +81,7 @@ int main() {
 		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		
 		;
 
 		testcase.setname("Play with smart guesser");
@@ -85,7 +90,12 @@ int main() {
 		for (uint i=0; i<100; ++i) {
 			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=10, true);  // smarty should always win in at most 10 turns!
 		}
-
+		for (uint i=0; i<100; ++i) {
+			testcase.CHECK_EQUAL(play(randy, smarty, 3, 100)<=10, true);  // smarty should always win in at most 10 turns!
+		}		
+		for (uint i=0; i<100; ++i) {
+			testcase.CHECK_EQUAL(play(randy, smarty, 8, 100)<=10, true);  // smarty should always win in at most 10 turns!
+		}		
     grade = testcase.grade();
 	} else {
 		testcase.print_signal(signal);
